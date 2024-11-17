d8b62134678a2576448f5621df3eb832fe850864
guilleacq <g.acquistapace@correo.ucu.edu.uy>
2024-11-17 11:50:06 -0300

    checkpoint

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from '../../components/navigation/AppNavigator';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <AppNavigator />
-    </NavigationContainer>
+    // <NavigationContainer>
+    //   <AppNavigator />
+    // </NavigationContainer>
+    <View>
+      <Text>Hola</Text>
+    </View>
   );
 }
