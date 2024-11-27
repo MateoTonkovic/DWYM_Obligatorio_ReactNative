ce4663cf445d6bb80ce385baf0d9898d82de1f92
Ana Bulla <ana.bulla@correo.ucu.edu.uy>
2024-11-26 21:02:49 -0300

    arreglando input con teclado iOS

--- src/services/search.service.js
+++ src/services/search.service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const API_URL = 'http://192.168.1.36:3001/api';
+const API_URL = 'http://192.168.8.101:3000/api';
 
 // Función para obtener el token de autenticación
 const getAuthToken = async () => {
